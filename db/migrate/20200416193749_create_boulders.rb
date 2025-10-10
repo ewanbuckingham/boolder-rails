c79544a86a24c15908767e8142c6c47d33e51ebe
ewanbuckingham <ewan@ewanbuckingham.com>
2025-10-10 14:58:57 +0100

    Updating geography for postgis

--- db/migrate/20200416193749_create_boulders.rb
+++ db/migrate/20200416193749_create_boulders.rb
@@ -1,7 +1,8 @@
 class CreateBoulders < ActiveRecord::Migration[6.0]
   def change
     create_table :boulders do |t|
-      t.st_polygon :polygon, geographic: true, srid: 4326
+      # t.st_polygon :polygon, geographic: true, srid: 4326
+      t.geography :polygon, geographic: true, srid: 4326, type: "polygon"
       t.timestamps
     end
   end
